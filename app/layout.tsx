96ad9611b9d6608fca6425aa162f9c510667acab
saulojr8 <saulojr8@gmail.com>
2025-05-02 15:20:56 -0300

    feat: add Header component and banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1>Barber Shop</h1>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+import Header from "@/components/header"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Click me</Button>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Saulo!</h2>
+        <p>Sexta -feira, 02 de maio.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Buscar por serviços ou barbearias" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Agende nos melhores com FSW Barber"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
